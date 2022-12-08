d0d8d2807b9e27a8ecd1f272352d39255ddce720
Masachusets <apofeoz@tut.by>
2022-12-08 23:18:11 +0300

    all commit

--- async_swapi_tech.py
+++ async_swapi_tech.py
@@ -6,15 +6,14 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from more_itertools import chunked
 
 from models import Base, Character
-from url import URL
-import async_swapi_tech
 
 
+URL2 = 'https://www.swapi.tech/'
 PG_DSN = 'postgresql+asyncpg://admin:1111@127.0.0.1:5433/aio_db'
 engine = create_async_engine(PG_DSN)
 Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-CHUNK_SIZE = 82
+CHUNK_SIZE = 83
 
 
 async def chunked_async(async_iter, size):
@@ -32,14 +31,14 @@ async def chunked_async(async_iter, size):
 
 
 async def res_to_data(json_res):
-    return {'name': json_res.get('name'),
-            'birth_year': json_res['birth_year'],
-            'gender': json_res['gender'],
-            'height': json_res['height'],
-            'mass': json_res['mass'],
-            'eye_color': json_res['eye_color'],
-            'hair_color': json_res['hair_color'],
-            'skin_color': json_res['skin_color']}
+    return {'name': json_res['result']['properties']['name'],
+            'birth_year': json_res['result']['properties']['birth_year'],
+            'gender': json_res['result']['properties']['gender'],
+            'height': json_res['result']['properties']['height'],
+            'mass': json_res['result']['properties']['mass'],
+            'eye_color': json_res['result']['properties']['eye_color'],
+            'hair_color': json_res['result']['properties']['hair_color'],
+            'skin_color': json_res['result']['properties']['skin_color']}
 
 
 async def get_extra_fields(url_list, session):
@@ -47,24 +46,15 @@ async def get_extra_fields(url_list, session):
     for url in url_list:
         async with session.get(url) as response:
             json_res = await response.json()
-            name_list.append(json_res['name'])
-    return ', '.join(name_list)
-
-
-async def get_films(url_list, session):
-    name_list = []
-    for url in url_list:
-        async with session.get(url) as response:
-            json_res = await response.json()
-            name_list.append(json_res['title'])
+            name_list.append(json_res['result']['properties']['name'])
     return ', '.join(name_list)
 
 
 async def get_person(people_id: int, session: ClientSession):
     print(f'begin {people_id}')
-    async with session.get(f'{URL}api/people/{people_id}') as response:
+    async with session.get(f'{URL2}api/people/{people_id}') as response:
         json_res = await response.json()
-        if json_res.get('detail', False):
+        if json_res.get('message') != 'ok':
             return {'id': people_id,
                     'name': 'n/a',
                     'birth_year': 'n/a',
@@ -80,20 +70,19 @@ async def get_person(people_id: int, session: ClientSession):
                     'starships': 'n/a',
                     'vehicles': 'n/a',
                     }
-        json_data = await res_to_data(json_res)
+        json_data = await res_to_data(json_res) | {'films': 'n/a',
+                                                   'species': 'n/a',
+                                                   'starships': 'n/a',
+                                                   'vehicles': 'n/a'}
         json_data['id'] = people_id
-        json_data['films'] = await get_films(json_res['films'], session)
-        json_data['homeworld'] = await get_extra_fields([json_res['homeworld']], session)
-        json_data['species'] = await get_extra_fields(json_res['species'], session)
-        json_data['starships'] = await get_extra_fields(json_res['starships'], session)
-        json_data['vehicles'] = await get_extra_fields(json_res['vehicles'], session)
+        json_data['homeworld'] = await get_extra_fields([json_res['result']['properties']['homeworld']], session)
     print(f'end {people_id}')
     return json_data
 
 
 async def get_people():
     async with ClientSession() as session:
-        for chunk in chunked(range(1, 83), CHUNK_SIZE):
+        for chunk in chunked(range(1, 10), CHUNK_SIZE):
             coroutines = [get_person(people_id=i, session=session) for i in chunk]
             results = await asyncio.gather(*coroutines)
             for item in results:
@@ -102,7 +91,7 @@ async def get_people():
 
 async def insert_people(people_chunk):
     async with Session() as session:
-        session.add_all([Character(**item) for item in people_chunk])
+        session.add_all([print(Character(**item)) for item in people_chunk])
         await session.commit()
 
 
@@ -120,9 +109,5 @@ async def main():
 
 
 start = datetime.datetime.now()
-# asyncio.run(main())
-if URL:
-    asyncio.run(main())
-else:
-    asyncio.run(async_swapi_tech.main())
+asyncio.run(main())
 print(datetime.datetime.now() - start)
